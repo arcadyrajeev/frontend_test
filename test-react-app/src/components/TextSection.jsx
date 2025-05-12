3ecca80614b2d0ea8b6a19f06cd8d9dcf0cb4ede
Rajeev <bitmasteratj@gmail.com>
2025-05-12 07:10:52 +0530

    new

---
+++ test-react-app/src/components/TextSection.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import React from "react";
+import "../styles/Home.css";
+
+export default function HomeHero() {
+  return (
+    <div className="text-section">
+      <div className="text-container"></div>
+    </div>
+  );
+}
